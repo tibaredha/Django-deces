52769b63ed2a214c262e8a87b68796e81ef5cced
tibaredha <tibaredha@yahoo.fr>
2020-11-06 08:26:26 +0100

    maj add comboboxes

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,24 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
+	
+	# FRESHMAN = 'FR'
+    # SOPHOMORE = 'SO'
+    # JUNIOR = 'JR'
+    # SENIOR = 'SR'
+    # GRADUATE = 'GR'
+    YEAR_IN_SCHOOL_CHOICES = [
+        ('FR', 'Freshman'),
+        ('SO', 'Sophomore'),
+        ('JR', 'Junior'),
+        ('SR', 'Senior'),
+        ('GR', 'Graduate'),
+    ]
+    year_in_school = models.CharField(
+        max_length=10,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default='SO',
+    )
     def __str__(self):
 	    return self.choice_text	
 	
